d55bc4ad2c2f74fcb4fecfac2401d9555a8a7043
Sushant Swamy <sushant.swamy@techspian.com>
2021-10-19 19:40:21 +0530

    initial commit done

---
+++ UdemyPractice/API/Controllers/DepartmentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route(template:"[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Get All Students");
+        }
+
+        [HttpGet(template:"{code}")]
+        public IActionResult GetA(string code)
+        {
+            return Ok("get this" + code + "department data");
+        }
+        
+        [HttpPost]
+        public IActionResult Insert()
+        {
+            return Ok("Insert new department");
+        }
+        
+        [HttpPut(template:"{code}")]
+        public IActionResult Update(string code)
+        {
+            return Ok("Update this" + code + "department data");
+        }
+        
+        [HttpDelete(template:"{code}")]
+        public IActionResult Delete(string code)
+        {
+            return Ok("Delete this" + code + "department data");
+        }
+    }
+}
